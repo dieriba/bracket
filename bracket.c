18ef91c9685a0908ba19ae2a09b03a7b1f6df402
Dieriba TOURE <dtoure@e2r11p12.clusters.42paris.fr>
2023-05-12 01:22:47 +0200

    fixing binary read

---
+++ bracket.c
@@ -0,0 +1,34 @@
+# include <unistd.h>
+# include <stdio.h>
+
+int check_if_valid_bracket(const char *msg, char to_find, int i)
+{
+    while (msg[++i])
+    {
+        if (msg[i] == '(' || msg[i] == '{' || msg[i] == '[')
+        {
+            i = check_if_valid_bracket(msg, (msg[i] % 2 ? msg[i] + 2 : msg[i] + 1), i);
+            if (i == -1)
+                return -1;
+        }
+        else if (msg[i] == ')' || msg[i] == '}' || msg[i] == ']')
+        {
+            if (msg[i] != to_find)
+                return -1;
+            return i;
+        }
+    }
+    if (msg[i] != to_find) return -1;
+    return 0;
+}
+
+int main (int argc, char **argv)
+{
+    for (int i = 1; i < argc; i++)
+    {
+        if (check_if_valid_bracket(argv[i], 0, -1) == 0)
+            printf("OK\n");
+        else
+            printf("Error\n");
+    }
+}
